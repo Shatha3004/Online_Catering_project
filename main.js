756f25ba2ae68b5c86c608107113de281bbef9a0
Shatha3004 <Shatha2004@gmail.com>
2024-07-22 22:37:24 +0530

    comit

---
+++ main.js
@@ -0,0 +1,77 @@
+
+// /first
+    var lay=document.querySelector(".overlay")
+    var box1=document.getElementById("first")
+
+function show1()
+{
+        lay.style.display="block"
+        box1.style.display="block"
+}
+
+
+function hide1()
+{
+    lay.style.display="none"
+    box1.style.display="none"
+}
+
+
+
+// second
+var lay=document.querySelector(".overlay")
+var box2=document.getElementById("second")
+
+function show2()
+{
+    lay.style.display="block"
+    box2.style.display="block"
+}
+
+
+function hide2()
+{
+    lay.style.display="none"
+    box2.style.display="none"
+}
+
+
+
+// third
+
+var lay=document.querySelector(".overlay")
+var box3=document.getElementById("third")
+
+function show3()
+{
+    lay.style.display="block"
+    box3.style.display="block"
+}
+
+
+function hide3()
+{
+    lay.style.display="none"
+    box3.style.display="none"
+}
+
+
+
+
+// fourth
+
+var lay=document.querySelector(".overlay")
+var box4=document.getElementById("fourth")
+
+function show4()
+{
+    lay.style.display="block"
+    box4.style.display="block"
+}
+
+
+function hide4()
+{
+    lay.style.display="none"
+    box4.style.display="none"
+}
